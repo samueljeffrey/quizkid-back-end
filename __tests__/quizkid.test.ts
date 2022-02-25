0dbdbd3161505aa9c4951cf6d0702017352b03d7
Samuel Jeffrey <samuel.jeffrey@outlook.com>
2022-02-25 14:40:42 +0000

    add initial setup from mocha docs to test file

---
+++ __tests__/quizkid.test.ts
@@ -0,0 +1,9 @@
+let assert = require("assert");
+
+describe("Array", () => {
+  describe("indexOf", () => {
+    it("should return -1 when no value", () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
