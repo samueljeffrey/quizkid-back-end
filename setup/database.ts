807d7d8947435257b6d7a0555acc4a39f9b81f97
Samuel Jeffrey <samuel.jeffrey@outlook.com>
2022-02-25 14:56:59 +0000

    add and complete .env file

---
+++ setup/database.ts
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const connectDB = () => {
+  mongoose
+    .connect(process.env.DB_URL)
+    .then(() => {
+      console.log("MongoDB Connected");
+    })
+    .catch((error?: object) => {
+      console.log(error);
+    });
+};
+
+const closeDB = () => {
+  mongoose.connection
+    .close()
+    .then(() => {
+      console.log("MongoDB Closed");
+    })
+    .catch((error?: object) => {
+      console.log(error);
+    });
+};
+
+module.exports = { connectDB, closeDB };
